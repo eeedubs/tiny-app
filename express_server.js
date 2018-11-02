56f38c36df75894bb784f4db12c54840356865d3
Eric Wilson <ericswilson94@gmail.com>
2018-11-01 18:14:43 -0700

    Added user registration, updated user login/logout

--- express_server.js
+++ express_server.js
@@ -14,6 +14,19 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-fun"
+  }
+}
+
 function generateRandomString() {
   let r = Math.random().toString(36).substring(2, 8);
   return r;
@@ -35,12 +48,39 @@ app.get("/hello", (req, res) => {
 });
 // sends the text in HTML format to the page 
 
+app.get("/register", (req, res) => {
+  if (req.cookies){
+    const value = req.cookies["user_id"];
+    let templateVars = {
+      "userID": value
+    };
+    res.render("urls_register", templateVars);
+  } else {
+    res.render("urls_register");
+  }
+});
+
+app.get("/login", (req, res) => {
+  if (req.cookies){
+    const value = req.cookies["user_id"];
+    let templateVars = {
+      "userID": value
+    };
+    res.render("urls_login", templateVars);
+  } else {
+    res.render("urls_login");
+  }
+});
+
 app.get("/urls/new", (req, res) => {
   if (req.cookies){
-    const value = req.cookies["username"];
-    res.render("urls_new", value);
+    const value = req.cookies["user_id"];
+    let templateVars = {
+      userID: value
+    };
+    res.render("urls_new", templateVars);
   } else {
-    res.render("urls_new");
+  res.render("urls_new");
   }
 });
 // renders to urls_new
@@ -50,18 +90,21 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:id", (req, res) => { // :id could be named anything
   let id = req.params.id; // get the id from the requirement parameters
   if (req.cookies){
-    const value = req.cookies["username"];
+    console.log("cookie was acquired");
+    const value = req.cookies["user_id"];
+    console.log(value);
     let templateVars = {
-      shortURL: id,
-      longURL: urlDatabase[id],
-      username: value
+      "shortURL": id,
+      "longURL": urlDatabase[id],
+      "userID": value
     };
     res.render("urls_show", templateVars)
   } else {
+    console.log("cookie was not acquired");
     let templateVars = { // templateVars gets rendered to urls_show.ejs
-      shortURL: id,
-      longURL: urlDatabase[id],
-      username: ""
+      "shortURL": id,
+      "longURL": urlDatabase[id],
+      "userID": ""
     };
     res.render("urls_show", templateVars);
   }
@@ -69,33 +112,36 @@ app.get("/urls/:id", (req, res) => { // :id could be named anything
 // 
 
 app.get("/u/:shortURL", (req, res) => {
-  let shortURL = req.params.shortURL;
-  let longURL = urlDatabase[shortURL];
-  if (!urlDatabase[shortURL]){
-    res.redirect("/urls")
-  } else {
-    res.redirect(301, longURL);
+  let shortURL = req.params.shortURL; // self explanatory
+  let longURL = urlDatabase[shortURL]; // ""
+  if (!urlDatabase[shortURL]){ // if there is no shortURL in the URL database
+    res.redirect("/urls") // redirect to the URL index
+  } else {  // else 
+    res.redirect(301, longURL); // redirect to the URL
   }
 });
 // handles the LINKING: attachs the shortURL to localhost:8010/u/
 
-app.get("/urls", (req, res) => {
-  if(req.cookies){
-    const value = req.cookies["username"];
-    let templateVars = { 
+app.get("/urls", (req, res) => { // reads the /urls page
+  if(req.cookies){  // if the cookies are acquired
+    const value = req.cookies["user_id"]; // assign the cookies to a variable
+    console.log(value); 
+    let templateVars = { // let template vars contain the urls and the username
       urls: urlDatabase,
-      username: value
+      userID: value
     };
     res.render("urls_index", templateVars);
   } else{
     let templateVars = { 
       urls: urlDatabase,
-      username: ""
+      userID: ""
     };
     res.render("urls_index", templateVars);
   }
 });
-// passing the urls from urlDatabase to the urls_index.ejs file
+// Reads the /urls page: 
+
+// POST METHODS BELOW
 
 app.post("/urls", (req, res) => {
   const response = generateRandomString(); // assigns a constant to the randomly generated string
@@ -125,17 +171,115 @@ app.post("/urls/:id/update", (req, res) => {
 })
 // handles EDIT: editing the longURL to which a short ID routes.
 
+function validateUser(email, password){
+  var foundUser;
+  for(var key in users){
+    if(users[key].email === email && users[key].password === password){
+      foundUser = users[key];
+    }
+  }
+  return foundUser;
+}
+
+function validateEmail(email){
+  var foundEmail;
+  for (var key in users){
+    if (users[key].email === email){
+      foundEmail = users[key].email;
+    }
+  }
+  return foundEmail;
+}
+// Confirms if the email was found (true if string includes email, false otherwise)
+
+function validatePass(password){
+  var foundPass; 
+  for (var key in users){
+    if (users[key].password === password){
+      foundPass = users[key].password;
+    }
+  }
+  return foundPass;
+}
+// Confirms if the password was found (true if string includes password, false otherwise)
+
 app.post("/login", (req, res) => {
-  const value = req.body.username;
-  res.cookie('username', value);
-  res.redirect('/urls');
-});
+  let loginEmail = req.body.email; // gets the login email
+  let loginPass = req.body.password; // gets the login password
+  var checkEmail = validateEmail(loginEmail); // true or false
+  var checkPass = validatePass(loginPass); // true or false
+  var userID = validateUser(loginEmail, loginPass);
+  var id = false;
+  if (loginEmail){ // if the email has been entered
+    if (loginPass){ // if the password has been entered
+      if (checkEmail){
+        if (checkPass){
+          console.log("Logged in with " + loginEmail);
+          res.cookie('user_id', userID);
+          res.redirect('/urls');
+        } else {
+          res.status(400); // bad password
+          res.send("400 Bad Request Error: The password entered does not match the email.");
+        }
+      } else { // bad email
+        res.status(400);
+        res.send("400 Bad Request Error: This email is not in our records.");
+      }
+    } else {
+      res.status(400);
+      res.send("400 Bad Request Error: Missing the user's password.");
+    }
+  } else {
+    res.status(400);
+    res.send("400 Bad Request Error: Missing the user's email.");
+  }
+})
+
+// Handles LOGIN: logging in and storing the username 
 
 app.post("/logout", (req, res) => {
-  console.log("clearing cookie");
-  res.clearCookie('username');
+  console.log("Logged out");
+  res.clearCookie('user_id');
   res.redirect('/urls');
 })
+// Handles LOGOUT: logging out and clearing the cookie
+
+app.post("/register", (req, res) => {
+  console.log("Registering a new user.");
+  const randomID = "user" + Math.random().toString(36).substring(2, 8);
+  const userEmail = req.body.email;
+  const userPass = req.body.password;
+  // console.log(users[randomID].email);
+  if (!userEmail && !userPass){
+    res.status(400);
+    res.send("400 Bad Request Error: Missing email address and password.")
+  } else if (!userEmail){
+    res.status(400);
+    res.send("400 Bad Request Error: Email address is missing.");
+  } else if (!userPass){
+    res.status(400);
+    res.send("400 Bad Request Error: Password is missing.");
+  } else {
+    for (var user in users){
+      if (userEmail == users[user].email){
+        res.status(400);
+        res.send("400 Bad Request Error: User's email address is already in use.");
+      }
+    } 
+    users[randomID] = {
+      "id": randomID,
+      "email": userEmail,
+      "password": userPass
+    };
+    console.log("after user registration ;",users);
+    //console.log("userID: " + JSON.stringify(users[randomID]));
+    // console.log("userID: " + JSON.stringify(users));
+    res.cookie('user_id', randomID);
+    res.redirect('/urls');
+  }
+});
+// Posts the new randomized user ID, along with their e-mail and password, 
+// to a new key-value pair in the users object
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
